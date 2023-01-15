10fe2c0afb7beb795743644cdbb1cf5309749fa8
Pyrosome <valeriaburina@gmail.com>
2023-01-15 19:25:41 +0000

    Deploying to gh-pages from @ Pyrosome/goit-js-hw-09@b1f83bc71569e609aaa3fcd5bab1094546eaa503 🚀

---
+++ 01-color-switcher.3c00dc87.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){})),t.addEventListener("click",(function(){}));
+//# sourceMappingURL=01-color-switcher.3c00dc87.js.map
---
+++ 01-color-switcher.dc2ecfc8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){})),t.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.dc2ecfc8.js.map
