ad583a7aa85389ce34f1d74cbbd3de1423c66518
Gaultheria <valeriaburina@gmail.com>
2023-01-16 00:01:09 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,37 @@
-// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона < body >
-// на случайное значение используя инлайн стиль.
-// При нажатии на кнопку «Stop»,
-// изменение цвета фона должно останавливаться.
-// Учти, на кнопку «Start» можно нажать бесконечное количество раз.
-// Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна(disabled).
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const start = document.querySelector('[data-start]');
 const stop = document.querySelector('[data-stop]');
 
-start.addEventListener('click', onStart);
-function onStart() {}
+let colorChange;
 
-start.addEventListener('click', onStop);
-function onStop() {}
+// create interval and do enable-disable on stop click
+start.addEventListener('click', onStart);
+function onStart() {
+  start.disabled = true;
+  stop.disabled = false;
+
+  colorChange = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+
+  return colorChange;
+}
+
+// delete interval and do disable-enable on stop click
+stop.disabled = true;
+stop.addEventListener('click', onStop);
+function onStop() {
+  start.disabled = false;
+  stop.disabled = true;
+
+  clearInterval(colorChange);
+  console.log(`Interval with id ${colorChange} has stopped!`);
+}
+
+// just styling a bit
+start.classList.add('colorswitchers');
+stop.classList.add('colorswitchers');
+stop.style.marginLeft = '50%';
